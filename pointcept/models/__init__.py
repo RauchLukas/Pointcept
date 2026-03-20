669d886937cea5c2f2746fa949d2aac80dba555f
RauchLukas <lukas.rauch@unibw.de>
2026-03-20 17:24:39 +0100

    activate swin3d training (reqiered the use of Minkwski Engin in Docker Container)

--- pointcept/models/__init__.py
+++ pointcept/models/__init__.py
@@ -12,7 +12,7 @@ from .spvcnn import *
 from .octformer import *
 from .oacnns import *
 
-# from .swin3d import *
+from .swin3d import *
 
 # Semantic Segmentation
 from .context_aware_classifier import *
